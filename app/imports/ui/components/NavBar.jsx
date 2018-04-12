65cff793404f8c608cc024d413cffd2a2a7de115
aidan_akamine <aidana@hawaii.edu>
2018-04-12 04:08:27 -1000

    user_profile

--- app/imports/ui/components/NavBar.jsx
+++ app/imports/ui/components/NavBar.jsx
@@ -34,6 +34,7 @@ class NavBar extends React.Component {
             <Dropdown text={this.props.currentUser} pointing="top right" icon={'user'}>
               <Dropdown.Menu>
                 <Dropdown.Item icon="sign out" text="Sign Out" as={NavLink} exact to="/signout"/>
+                <Dropdown.Item icon="edit profile" text = "View profile" as={NavLink} exact to="/profileview"/>
               </Dropdown.Menu>
             </Dropdown>
           )}
